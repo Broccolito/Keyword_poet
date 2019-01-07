74e02d95b3057e3340cab8c46dc563712825c55b
Wanjun Gu <wanjungu@yahoo.com>
2019-01-07 07:46:12 -0800

    Add files via upload

---
+++ start.R
@@ -0,0 +1,25 @@
+get_directory = function(){
+  args <- commandArgs(trailingOnly = FALSE)
+  file <- "--file="
+  rstudio <- "RStudio"
+  match <- grep(rstudio, args)
+  if(length(match) > 0){
+    return(dirname(rstudioapi::getSourceEditorContext()$path))
+  }else{
+    match <- grep(file, args)
+    if (length(match) > 0) {
+      return(dirname(normalizePath(sub(file, "", args[match]))))
+    }else{
+      return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+    }
+  }
+}
+
+wd = get_directory()
+setwd(wd)
+
+suppressPackageStartupMessages({
+  source("main.R")
+})
+
+runApp(getwd())
